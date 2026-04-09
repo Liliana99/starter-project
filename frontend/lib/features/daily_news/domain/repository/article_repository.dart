6125fbe46d896dc54733e227a7442bec7c681368
Liliana99 <marialiji@gmail.com>
2026-04-09 02:09:07 +0200

    feat: complete article repository implementation and local storage logic

--- frontend/lib/features/daily_news/domain/repository/article_repository.dart
+++ frontend/lib/features/daily_news/domain/repository/article_repository.dart
@@ -2,13 +2,15 @@ import 'package:news_app_clean_architecture/core/resources/data_state.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 
 abstract class ArticleRepository {
-  // API methods
   Future<DataState<List<ArticleEntity>>> getNewsArticles();
 
-  // Database methods
-  Future < List < ArticleEntity >> getSavedArticles();
+  Future<List<ArticleEntity>> getSavedArticles();
+  Future<void> saveArticle(ArticleEntity article);
+  Future<void> removeArticle(ArticleEntity article);
 
-  Future < void > saveArticle(ArticleEntity article);
+  Future<void> publishArticle(ArticleEntity article);
+  Stream<List<ArticleEntity>> searchArticles(String query);
 
-  Future < void > removeArticle(ArticleEntity article);
+  Future<List<ArticleEntity>> getRemotePublishedArticles();
+  Future<void> deleteRemoteArticle(String id);
 }
